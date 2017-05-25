19b2ac6b7786662ae7acf71c8c037756f15390f4
jinwoo.yuk <esixman@gmail.com>
2017-05-25 16:08:54 +0900

    .

--- src/app/views/contents/login/login.component.ts
+++ src/app/views/contents/login/login.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { AppConstants } from '../../../../core/constants';
 
 @Component({
     selector: 'app-login',
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles: [ './login.component.css' ]
 })
 export class LoginComponent implements OnInit {
 
-    constructor(public router: Router) { }
+    constructor(public router: Router, private translate: TranslateService) {
+    }
 
-    ngOnInit() { }
+    ngOnInit() {
+    }
 
-    onLoggedin() {
-        localStorage.setItem(AppConstants.appAuthKey, 'true');
+    onLogin() {
+      localStorage.setItem(AppConstants.appAuthKey, 'true');
+      this.router.navigate(['/dashboard']);
+    }
+
+    setTranslate(language: string) {
+      this.translate.use(language);
     }
 
 }
--- src/app/views/contents/login/login.module.ts
+++ src/app/views/contents/login/login.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    TranslateModule
   ],
-  declarations: [LoginComponent]
+  declarations: [ LoginComponent ]
 })
 export class LoginModule { }
